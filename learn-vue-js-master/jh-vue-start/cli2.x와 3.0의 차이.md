b03bbec21c3c3d28482f4c5ee1b3607b632de357
manbalboy <manbalboy@hanmail.net>
2021-02-13 20:09:38 +0900

    vue cli/

---
+++ learn-vue-js-master/jh-vue-start/cli2.x와 3.0의 차이.md
@@ -0,0 +1,10 @@
+[Vue CLI 2.x]
+vue init '프로젝트 템플릿 유형' '프로젝트 폴더 위치'
+vue inti webpack-simple '프로젝트 폴더 위치'
+
+[Vue CLI 3.x]
+vue create '프로젝트 폴더 위치'
+
+실행
+ $ cd vue-cli
+ $ npm run serve
---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{str}}</p>
+        <app-header></app-header>
+    </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data : function (){
+        return {
+            str : 'hi'
+        }
+    },
+
+    components : {
+        'app-header' : AppHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
