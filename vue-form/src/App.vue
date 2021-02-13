ee63d82aa52d8379b5e2383ef808eb5ca5841667
manbalboy <manbalboy@hanmail.net>
2021-02-13 22:03:44 +0900

    싱글페이지 엑시오스/이벤트/ start 최종 학습

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <form v-on:submit.prevent="submitForm">
+        <div>
+            <label for="username">id:</label>
+            <input id="username" type="text" v-model="username">
+        </div>
+        <div>
+            <label for="password">pw:</label>
+            <input id="password" type="password" v-model="password">
+        </div>
+        <button type="submit">login</button>
+    </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data : function () {
+            return {
+                username : "",
+                password : ""
+            }
+        },
+
+        methods: {
+            submitForm : function () {
+                // e.preventDefault();
+                alert(1);
+                console.log(this.username, this.password);
+                var data = {
+                    username : this.username,
+                    password : this.password
+                }
+
+
+                axios.post('https://jsonplaceholder.typicode.com/users',data)
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error));
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
